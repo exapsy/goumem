dab6d5f167fd838327459e081e3a575ed20b5d1f
Apostolis Anastasiou <apostolis.anastasiou.alpha@gmail.com>
2023-12-26 05:14:42 +0200

    Change test to capture cases of freed bytes

--- pool/pool_test.go
+++ pool/pool_test.go
@@ -4,12 +4,13 @@ import "testing"
 
 func TestPool(t *testing.T) {
 	pool, err := New(Options{
-		Size: 1024,
+		Size: 8,
 	})
 	if err != nil {
 		t.Fatalf("New() error = %v", err)
 	}
 
+	// Allocate 2 times 4 bytes (8 bytes total)
 	addr, err := pool.Alloc(4)
 	if err != nil {
 		t.Fatalf("Alloc() error = %v", err)
@@ -20,7 +21,35 @@ func TestPool(t *testing.T) {
 		t.Errorf("Alloc() = %v, want 456", addr.Int())
 	}
 
+	addr2, err := pool.Alloc(4)
+	if err != nil {
+		t.Fatalf("Alloc() error = %v", err)
+	}
+
+	addr2.Set(456)
+	if addr2.Int() != 456 {
+		t.Errorf("Alloc() = %v, want 456", addr2.Int())
+	}
+
+	// Free the first 4 bytes
 	err = pool.Free(addr, 4)
+	if err != nil {
+		t.Fatalf("Free() error = %v", err)
+	}
+
+	// Allocate 4 bytes again (should work since we freed 4 bytes)
+	addr3, err := pool.Alloc(4)
+	if err != nil {
+		t.Fatalf("Alloc() error = %v", err)
+	}
+
+	addr3.Set(456)
+	if addr3.Int() != 456 {
+		t.Errorf("Alloc() = %v, want 456", addr3.Int())
+	}
+
+	// Free the pool
+	err = pool.Close()
 	if err != nil {
 		t.Fatalf("Free() error = %v", err)
 	}
