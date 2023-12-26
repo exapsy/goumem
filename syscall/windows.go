02297a283f591cb1cc979041114e3e1797a1ddc3
Apostolis Anastasiou <apostolis.anastasiou.alpha@gmail.com>
2023-12-26 04:46:19 +0200

    Add memory allocation in windows

--- syscall/windows.go
+++ syscall/windows.go
@@ -8,6 +8,23 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+func Mmap(size uintptr) (uintptr, error) {
+	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
+	virtualAlloc := kernel32.NewProc("VirtualAlloc")
+
+	r1, _, err := virtualAlloc.Call(
+		0,
+		size,
+		uintptr(0x1000), // MEM_COMMIT
+		uintptr(0x04),   // PAGE_READWRITE
+	)
+	if r1 == 0 {
+		return 0, fmt.Errorf("failed to make VirtualAlloc syscall: %w", err)
+	}
+
+	return r1, nil
+}
+
 func Free(addr, size uintptr) error {
 	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
 	virtualFree := kernel32.NewProc("VirtualFree")
